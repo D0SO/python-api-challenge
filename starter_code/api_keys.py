b8d77c3aed8b904bba3edb9d8d8ef3aef475c48a
D0SO <danielptwa@gmail.com>
2021-03-03 00:59:37 +0800

    done

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "9f18abfd99d897f85ae6155a9007ebca"
+weather_api_key = "9ac2f04c28da43b6b456fdfc97b6c37b"
 
 # Google API Key
 g_key = "AIzaSyB4eF432Cd3PixUWWCrryEn4l_OMmnVkqw"
